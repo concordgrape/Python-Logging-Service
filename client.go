5b929f9d6920de6f5ab1ecadb021bf3de09b7c59
Sky Roth <55454027+concordgrape@users.noreply.github.com>
2021-02-22 15:12:02 -0500

    Creation of files

---
+++ client.go
@@ -0,0 +1,13 @@
+/*
+*	CLIENT.GO
+*	CREATED ON: 02/22/21
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("First program!")
+}
---
+++ server.py
@@ -0,0 +1,6 @@
+#
+#	SERVER.PY
+#	CREATED ON: 02/22/21
+#
+
+print("Hello World!")
